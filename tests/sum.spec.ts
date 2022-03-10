7eaa5f135f404c715a150b81879b3af2e21098de
John BRETAS <john_bretas@carrefour.com>
2022-03-10 12:49:16 -0300

    fix: fix sum test description

--- tests/sum.spec.ts
+++ tests/sum.spec.ts
@@ -11,7 +11,7 @@ describe('Sum', () => {
     expect(result).toBe(6)
   })
 
-  it('allows negative numbers three numbers together', () => {
+  it('allows negative numbers to be summed together', () => {
     const sut = makeSut()
     const result = sut.execute(-1, -2, 3)
     expect(result).toBe(0)
